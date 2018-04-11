f9c8c262c976422cdaf3b6114ce645564e3a7d00
Cesar Romero <cesarliws@gmail.com>
2018-04-11 15:47:31 -0300

    + Commit inicial: Game Engine A implementado

---
+++ RockPaperScissors/Classes/GameEngine.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace RockPaperScissors.Classes
+{
+    class Play
+    {
+        public Play(string[] values)
+        {
+            var Player = values[0];
+            var Strategy = values[1];
+
+            if (!IsValid())
+            {
+                throw new NoSuchStrategyError(
+                    String.Format("Strategy {0} is not valid!", Strategy));
+            }
+
+        }
+        public string Player { get; set; }
+
+        public char Strategy
+        {
+            get { return Strategy; }
+            set { Strategy = char.ToUpperInvariant(value); }
+        }
+
+        public bool IsValid()
+        {
+            return (this.Strategy == 'R' || this.Strategy == 'P' || this.Strategy == 'S');
+        }
+
+        public bool Compare(Play otherPlay)
+        {
+            return (
+                (this.Strategy == 'R' && otherPlay.Strategy == 'S') ||
+                (this.Strategy == 'S' && otherPlay.Strategy == 'P') ||
+                (this.Strategy == 'P' && otherPlay.Strategy == 'R') ||
+                this.Strategy == otherPlay.Strategy
+            );
+        }
+    }
+
+    class GameEngine
+    {
+        private Play FirstPlay;
+        private Play SecondPlay;
+
+        private void CheckPlayCount(string[][] values)
+        {
+            if (values.Length != 2)
+            {
+                throw new WrongNumberOfPlayersError("The number of players is not equal to 2!");
+            }
+        }
+
+        public string rps_game_winner(string[][] values)
+        {
+            CheckPlayCount(values);
+
+            FirstPlay = new Play(values[0]);
+            SecondPlay = new Play(values[1]);
+
+            if (FirstPlay.Compare(SecondPlay))
+            {
+                return FirstPlay.Player;
+            }
+            else
+            {
+                return SecondPlay.Player;
+            }
+
+        }
+    }
+}
--- RockPaperScissors/Program.cs
+++ RockPaperScissors/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace RockPaperScissors
 {
@@ -6,7 +7,41 @@ namespace RockPaperScissors
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string inputString;
+            string[] rps = new string[3] { "r", "p", "s" };
+
+            int randomNumber;
+
+            Random ranNumberGenerator = new Random();
+            randomNumber = ranNumberGenerator.Next(4);
+
+            bool testCondition = true;
+
+            while (testCondition)
+            {
+                Console.WriteLine("Enter r, p or c");
+                inputString = Console.ReadLine();
+
+                if (inputString != null && (inputString.Length == 1 && rps.Contains(inputString)))
+                {
+                    randomNumber = +ranNumberGenerator.Next(4);
+
+                    if (rps[randomNumber].Equals(inputString))
+                        Console.WriteLine("Draw");
+                    else
+                        Console.WriteLine("Lose");
+
+                    testCondition = false;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Entry");
+                }
+
+            }
+
+            Console.Read();
+
         }
     }
 }
